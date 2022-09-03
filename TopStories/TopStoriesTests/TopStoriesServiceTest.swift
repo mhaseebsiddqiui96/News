c6cf921fbeb22eb8941cfaf2335d69ea661ffb67
Muhammad Haseeb Siddiqui <mhaseebsiddiqui@folio3.com>
2022-09-04 00:29:20 +0500

    creat test file to test top stories service

---
+++ TopStories/TopStoriesTests/TopStoriesServiceTest.swift
@@ -0,0 +1,27 @@
+//
+//  TopStoriesTests.swift
+//  TopStoriesTests
+//
+//  Created by Muhammad Haseeb Siddiqui on 9/4/22.
+//
+
+import XCTest
+@testable import TopStories
+
+class TopStoriesServiceTest: XCTestCase {
+
+    var sut: TopStoriesService!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+       
+    }
+    
+}
