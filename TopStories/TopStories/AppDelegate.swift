46d09f70c6e5119112b1f483c22c1a0488472942
Muhammad Haseeb Siddiqui <mhaseebsiddiqui@folio3.com>
2022-09-03 16:59:52 +0500

    Create project files, set iOS version to 12 and removed unnecessary files

---
+++ TopStories/TopStories/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TopStories
+//
+//  Created by Muhammad Haseeb Siddiqui on 9/3/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        return true
+    }
+
+
+}
+
