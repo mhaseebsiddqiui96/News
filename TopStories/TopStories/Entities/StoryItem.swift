9f23b3261cc93df939f9cd9ffd1e9b33c19d9cd6
Muhammad Haseeb Siddiqui <mhaseebsiddiqui@folio3.com>
2022-09-05 02:24:10 +0500

    create story item entity model

--- TopStories/TopStories/Entities/StoryItem.swift
+++ TopStories/TopStories/Entities/StoryItem.swift
@@ -8,5 +8,44 @@
 import Foundation
 
 struct StoryItem: Equatable {
+    
     let id: UUID
+    let section : String?
+    let subsection : String?
+    let title : String?
+    let abstract : String?
+    let url : String?
+    let uri : String?
+    let byline : String?
+    let itemType : String?
+    let updatedDate : Date?
+    let createdDate : Date?
+    let publishedDate : Date?
+    let materialTypeFacet : String?
+    let kicker : String?
+    let des_facet : [String]?
+    let org_facet : [String]?
+    let per_facet : [String]?
+    let geo_facet : [String]?
+    let multimedia : [Multimedia]?
+    let short_url : String?
+
+    
+    
+    struct Multimedia {
+        let url : String?
+        let format : String?
+        let height : Int?
+        let width : Int?
+        let type : String?
+        let subtype : String?
+        let caption : String?
+        let copyright : String?
+        
+    }
+    
+    static func == (lhs: StoryItem, rhs: StoryItem) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
 }
