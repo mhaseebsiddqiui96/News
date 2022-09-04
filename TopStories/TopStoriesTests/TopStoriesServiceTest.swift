09bfc5221719bafc340258dd0421231918e5892c
Muhammad Haseeb Siddiqui <mhaseebsiddiqui@folio3.com>
2022-09-04 21:37:24 +0500

    refactor the sut creation in factory method

--- TopStories/TopStoriesTests/TopStoriesServiceTest.swift
+++ TopStories/TopStoriesTests/TopStoriesServiceTest.swift
@@ -12,21 +12,22 @@ class TopStoriesServiceTest: XCTestCase {
     
 
     func test_init_doesNotRequestDataFromClient() throws {
-        let client = HTTPClientSpy()
-        _ = TopStoriesService(client: client, urlRequest: URLRequest(url: URL(string: "https://some-url.com")!))
         
+        let (client, _) = makeSUT()
         XCTAssert(client.requestedURL.isEmpty)
         
     }
     
     func test_fetch_requestDataFromClient() throws {
-        let client = HTTPClientSpy()
-        let sut = TopStoriesService(client: client, urlRequest: URLRequest(url: URL(string: "https://some-url.com")!))
+        
+        let url = URL(string: "https://test1.com")!
+        let (client, sut) = makeSUT(url: url)
         
         sut.fetch(completion: {_ in })
         
-        XCTAssert(!client.requestedURL.isEmpty)
+        XCTAssertEqual(client.requestedURL.first?.url, url)
     }
+    
     
     //MARK: - Helpers
     class HTTPClientSpy: HTTPClient {
@@ -37,6 +38,14 @@ class TopStoriesServiceTest: XCTestCase {
             requestedURL.append(urlRequest)
         }
         
+    }
+    
+    // factory method to create sut. it will help if the creation of changes then we only need to update this method rest of the test will not be effected
+    func makeSUT(url: URL = URL(string: "https://some-url.com")!) -> (client: HTTPClientSpy, service: TopStoriesService) {
+        let client = HTTPClientSpy()
+        let sut = TopStoriesService(client: client, urlRequest: URLRequest(url: url))
+        
+        return (client, sut)
     }
     
 }
